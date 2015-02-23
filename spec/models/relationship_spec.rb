74e13ceffc23b141832b79172b6f90f02eee2cc0
katarina <golbang.k@gmail.com>
2015-02-23 21:57:39 +0100

    test relationship model

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Relationship do
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @group = FactoryGirl.create(:group)
+  end
+
+  it 'connects the user with the group' do
+    rel = Relationship.new
+    rel.group = @group
+    rel.user = @user
+    rel.save!
+
+    expect(@group.users).to include(@user)
+    expect(@user.groups).to include(@group)
+  end
+
 end
